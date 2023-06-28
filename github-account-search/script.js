cc913301e845d829bf6cfb31c869bf68136e6ea2
Joshua Varghese Thomji <69610717+josh1221wa@users.noreply.github.com>
2023-06-28 11:13:35 +0530

    Updated github-account-search
    Fixed code and added doceumentation to files

--- github-account-search/script.js
+++ github-account-search/script.js
@@ -1,12 +1,12 @@
-const APIURL = "https://api.github.com/users/";
+const APIURL = "https://api.github.com/users/";   // The Github users API
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-async function getUser(username) {
+async function getUser(username) {    // The function to get user data
   try {
-    const { data } = await axios(APIURL + username);
+    const { data } = await axios(APIURL + username);    // Collects the JSON from the API call and converts to object
     createUserCard(data);
     getRepos(username);
   } catch (err) {
@@ -16,10 +16,9 @@ async function getUser(username) {
   }
 }
 
-async function getRepos(username) {
+async function getRepos(username) {   // The function to collect repos made by a user, sorted by created date
   try {
     const { data } = await axios(APIURL + username + "/repos?sort=created");
-
     addReposToCard(data);
   } catch (err) {
     createErrorCard("Problem fetching repos");
@@ -27,7 +26,8 @@ async function getRepos(username) {
 }
 
 function createUserCard(user) {
-  if (user.bio == null) user.bio = "Welcome to my Github Profile!";
+  if (user.bio == null) user.bio = "Welcome to my Github Profile!";   // If the user has no bio, it enters a generic bio
+  // Below it creates the html code to be inserted in between the original file to show the card
   const cardhtml = `
     <div class="card">
     <div>
@@ -47,9 +47,11 @@ function createUserCard(user) {
     </div>
   </div>
   `;
-  main.innerHTML = cardhtml;
+  main.innerHTML = cardhtml;    // Inserts the card inside main
 }
+
 function createErrorCard(msg) {
+  // Creates error card
   const cardHTML = `
       <div class="card">
           <h1>${msg}</h1>
@@ -57,7 +59,8 @@ function createErrorCard(msg) {
   `;
   main.innerHTML = cardHTML;
 }
-function addReposToCard(repos) {
+
+function addReposToCard(repos) {  // Insert the repos under the class repos
   const reposEl = document.getElementById("repos");
   repos.slice(0, 5).forEach((repo) => {
     const repoEl = document.createElement("a");
@@ -69,6 +72,8 @@ function addReposToCard(repos) {
     reposEl.appendChild(repoEl);
   });
 }
+
+// Is an event listener for Enter key
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const user = search.value;
